8e9c02079db5c0f83a0d79edd6824efcd66e5536
hakan.basturknewinn.com <forxynet@gmail.com>
2025-01-21 01:31:35 +0300

    complated

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -5,12 +5,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDwjcDwJ7SvrBOohEpcyttwWkYlMBRvqMM",
-  authDomain: "reading-list-app-d6e97.firebaseapp.com",
-  projectId: "reading-list-app-d6e97",
-  storageBucket: "reading-list-app-d6e97.firebasestorage.app",
-  messagingSenderId: "402995207890",
-  appId: "1:402995207890:web:76d1d8fa39193acee3d70b"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
